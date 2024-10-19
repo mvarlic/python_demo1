cdb7d61a24003971521a25a22fb50133b7a47edc
Miguel Vargas <mvarlic@gmail.com>
2024-10-18 20:58:45 -0500

    s

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def helloworld():
-    return "Hello World!"
+    return "Hello World 2!"
 if __name__ == "__main__":
     app.run()
 
