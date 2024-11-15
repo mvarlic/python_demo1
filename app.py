625d37ccddc8d72910a57ba9834f42f15c0e4fa2
Miguel Vargas <mvarlic@gmail.com>
2024-11-15 18:22:30 -0500

    feat: todo

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
+import os
 from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv()
+
+APP_PORT = os.getenv("APP_PORT")
 app = Flask(__name__)
 @app.route("/")
 def helloworld():
     return "Hello World 3!"
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=APP_PORT)
 
     
