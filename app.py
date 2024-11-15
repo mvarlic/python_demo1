cd39b3c72907ac97d9d833dcfdf858cc2c9cb58c
Miguel Vargas <mvarlic@gmail.com>
2024-11-15 18:31:11 -0500

    feat: listo!

--- app.py
+++ app.py
@@ -4,11 +4,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-APP_PORT = int(os.getenv("APP_PORT", 80))
+try:
+    APP_PORT = int(os.getenv("APP_PORT", 80))
+except ValueError:
+    raise ValueError("El puerto especificado en APP_PORT no es un número válido.")
+
 app = Flask(__name__)
+
 @app.route("/")
 def helloworld():
     return "Hello World 3!"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=APP_PORT)
 
